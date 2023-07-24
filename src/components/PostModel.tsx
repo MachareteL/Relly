c9d15e814780831069aa96c0e7327897f02c71f7
MachareteL <macharetelucas@gmail.com>
2023-07-24 09:36:36 -0300

    style: :lipstick: animation on the post list

--- src/components/PostModel.tsx
+++ src/components/PostModel.tsx
@@ -4,7 +4,7 @@ import { ProfileImage } from "./ProfileImage";
 import { LifebuoyIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { api } from "~/utils/api";
 import RellyButton from "./RellyButton";
-
+import { motion } from "framer-motion";
 export default function PostModel({
   id,
   user,
@@ -35,66 +35,73 @@ export default function PostModel({
   }
 
   return (
-    <li className="flex gap-4 overflow-hidden rounded-lg bg-[rgba(100,116,139,0.5)] px-4 pb-2 pt-4 backdrop-blur-3xl">
-      <Link href={`/profile/${user.id}`} className="h-fit">
-        <ProfileImage src={user.image} />
-      </Link>
-      <div className="flex flex-grow flex-col gap-1">
-        <div className="flex justify-between gap-1">
-          <Link href={`/profile/${user.id}`} className="font-bold">
-            {user.name}
-          </Link>
-          {createdByCurrentUser && (
-            <button onClick={() => handleDelete(id)}>
-              <TrashIcon className="h-5 w-5 cursor-pointer" />
-            </button>
-          )}
-        </div>
-        <p className="flex-grow whitespace-pre-wrap break-all">{content}</p>
-        <div className="flex justify-between text-center">
-          {/* <button
+    <motion.div
+      className="origin-bottom"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.3 }}
+    >
+      <li className="flex gap-4 overflow-hidden rounded-lg bg-[rgba(100,116,139,0.5)] px-4 pb-2 pt-4 backdrop-blur-3xl">
+        <Link href={`/profile/${user.id}`} className="h-fit">
+          <ProfileImage src={user.image} />
+        </Link>
+        <div className="flex flex-grow flex-col gap-1">
+          <div className="flex justify-between gap-1">
+            <Link href={`/profile/${user.id}`} className="font-bold">
+              {user.name}
+            </Link>
+            {createdByCurrentUser && (
+              <button onClick={() => handleDelete(id)}>
+                <TrashIcon className="h-5 w-5 cursor-pointer" />
+              </button>
+            )}
+          </div>
+          <p className="flex-grow whitespace-pre-wrap break-all">{content}</p>
+          <div className="flex justify-between text-center">
+            {/* <button
             onClick={() => handleRelly(id, 1)}
             disabled={addRelly.isLoading}
             className="flex items-center space-x-1 rounded-lg text-indigo-400 hover:text-slate-300"
-          >
-         </button> */}
-          <div className="flex w-full items-center justify-between sm:w-2/4 sm:pr-20">
-            <span className="flex items-center">
-              <LifebuoyIcon className="h-5 w-5" />
-              <p>{likeCount}</p>
-            </span>
-            <div className="flex items-center">
-              <RellyButton
-                handleRelly={handleRelly}
-                id={id}
-                likedByUser={likedByUser}
-                className="px-2"
-                onClickColor="ffegde"
-                value={1}
-              />
-              <RellyButton
-                handleRelly={handleRelly}
-                id={id}
-                likedByUser={likedByUser}
-                className="px-2"
-                onClickColor="ff0081"
-                value={5}
-              />
-              <RellyButton
-                handleRelly={handleRelly}
-                id={id}
-                likedByUser={likedByUser}
-                className="px-2"
-                onClickColor="ff0081"
-                value={15}
-              />
+            >
+          </button> */}
+            <div className="flex w-full items-center justify-between sm:w-2/4 sm:pr-20">
+              <span className="flex items-center">
+                <LifebuoyIcon className="h-5 w-5" />
+                <p>{likeCount}</p>
+              </span>
+              <div className="flex items-center">
+                <RellyButton
+                  handleRelly={handleRelly}
+                  id={id}
+                  likedByUser={likedByUser}
+                  className="px-2"
+                  onClickColor="ffegde"
+                  value={1}
+                />
+                <RellyButton
+                  handleRelly={handleRelly}
+                  id={id}
+                  likedByUser={likedByUser}
+                  className="px-2"
+                  onClickColor="ff0081"
+                  value={5}
+                />
+                <RellyButton
+                  handleRelly={handleRelly}
+                  id={id}
+                  likedByUser={likedByUser}
+                  className="px-2"
+                  onClickColor="ff0081"
+                  value={15}
+                />
+              </div>
             </div>
+            <span className="self-end text-xs text-gray-300">
+              -{createdAt.toDateString()}
+            </span>
           </div>
-          <span className="self-end text-xs text-gray-300">
-            -{createdAt.toDateString()}
-          </span>
         </div>
-      </div>
-    </li>
+      </li>
+    </motion.div>
   );
 }
