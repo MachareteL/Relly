ef7ce705abc885b2db6ff2fc941bad5beac7472a
MachareteL <macharetelucas@gmail.com>
2023-07-31 07:59:51 -0300

    feat adding relly view to the mobile version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,30 @@
 import { useSession } from "next-auth/react";
 import { api } from "~/utils/api";
-import { type FormEvent, useState } from "react";
+import { type FormEvent, useState, Fragment } from "react";
 import PostsList from "~/components/PostsList";
 import { LifebuoyIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import avif from "public/docs-dark@30.1a9f8cbf.avif";
 import type { NextPage } from "next";
 import TrendingPostsList from "~/components/TrendingPostsList";
+import { Dialog, Transition } from "@headlessui/react";
 
 const Home: NextPage = ({}) => {
   const { data } = useSession({
     required: true,
   });
   console.log(data);
-  
+
+  let [isOpen, setIsOpen] = useState(true);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
   const [content, setContent] = useState("");
   const ctx = api.useContext();
   const newPost = api.post.create.useMutation({
@@ -72,6 +83,10 @@ const Home: NextPage = ({}) => {
       </div>
       <div className="min-h-screen gap-4 p-4 lg:container md:grid md:grid-cols-12 lg:m-auto">
         <div className="md:col-span-8">
+          <div className="flex items-center rounded-lg border border-white border-opacity-60 bg-white bg-opacity-20 p-4 sm:hidden">
+            <LifebuoyIcon className="h-6" />
+            {user.data?.relliesAmmount} Rellies
+          </div>
           <div className="mb-4 rounded-lg border border-white border-opacity-60 bg-white bg-opacity-20 p-4">
             <form onSubmit={handleSubmit} className="flex flex-col gap-4">
               <textarea
@@ -99,7 +114,7 @@ const Home: NextPage = ({}) => {
           {/* trending */}
           <RecentPosts />
         </div>
-        <div className="hidden md:col-span-4 md:block md:space-y-4 -z-10">
+        <div className="-z-10 hidden md:col-span-4 md:block md:space-y-4">
           <div className="rounded-lg border border-white border-opacity-70 bg-white bg-opacity-20 p-4 backdrop-blur-xl">
             <h1 className="text-3xl font-bold">Balance</h1>
             <h1 className="flex items-center text-lg font-medium">
@@ -107,12 +122,73 @@ const Home: NextPage = ({}) => {
               {user.data?.relliesAmmount} Rellies
             </h1>
           </div>
-          <div className="h-full flex flex-col">
-            <span className="text-end italic">Last 48h most Rellyful posts</span>
+          <div className="flex h-full flex-col">
+            <span className="text-end italic">
+              Last 48h most Rellyful posts
+            </span>
             <RecentTopPosts />
           </div>
         </div>
       </div>
+      <>
+        <Transition appear show={isOpen} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={closeModal}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black bg-opacity-25" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-gray-900"
+                    >
+                      Welcome to relly!
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        To interact with others people's posts you will need
+                        rellys. <br />
+                        You will earn rellys every 5 minutes and everytime
+                        someone else relly to your post or follow you. <br />
+                        Read further in the <i>ABOUT</i> page, you find it on the menu.
+                      </p>
+                    </div>
+
+                    <div className="mt-4">
+                      <button
+                        type="button"
+                        className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                        onClick={closeModal}
+                      >
+                        Got it, thanks!
+                      </button>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition>
+      </>
     </>
   );
 };
